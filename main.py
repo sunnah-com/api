eb46e9d56353f4e914c75edf0994185106889a76
Glaisher <glaisher.wiki@gmail.com>
2020-06-06 23:29:54 +0500

    Use sqlalchemy automap and implement books and hadiths API (#15)
    * models: Make use of sqlalchemy automap
    
    * Implement books and hadiths API
    
    Added hadithTable dump as it was missing from initial
    dump so would need to import HadithTable first for this
    to work properly.

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 from flask import Flask, jsonify
-from models import db, HadithCollection
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
-db.init_app(app)
 
-hadiths = [
-    {'id': 1,
-     'collection_id': 1,
-     'collection': 'Sahih al-Bukhari',
-     'book_id': 1,
-     'book': 'Revelation',
-     'chapter_id': 1,
-     'chapter': 'How the Divine Revelation started being revealed to Allah\'s Messenger',
-     'text': 'I heard Allah\'s Messenger (ﷺ) saying, "The reward of deeds depends upon the intentions and every person will get the reward according to what he has intended. So whoever emigrated for worldly benefits or for a woman to marry, his emigration was for what he emigrated for."'}
-]
+from models import db, HadithCollection, Book, Hadith
 
 @app.route('/', methods=['GET'])
 def home():
@@ -23,19 +12,25 @@ def home():
 @app.route('/v1/collections', methods=['GET'])
 def api_collections():
     queryset = HadithCollection.query.all()
-    collections = [x.serialize() for x in queryset]
-    return jsonify(collections)
+    results = [x.serialize() for x in queryset]
+    return jsonify(results)
 
-@app.route('/v1/collections/<int:collection_id>', methods=['GET'])
-def api_collection(collection_id):
-    collection = HadithCollection.query.get_or_404(collection_id)
+@app.route('/v1/collections/<string:name>', methods=['GET'])
+def api_collection(name):
+    collection = HadithCollection.query.filter_by(name=name).first_or_404()
     return jsonify(collection.serialize())
 
-@app.route('/v1/collections/<int:collection_id>/books/<int:book_id>/hadiths', methods=['GET'])
-def api_hadiths(collection_id, book_id):
-    result = [hadith for hadith in hadiths if hadith['collection_id'] == collection_id and hadith['book_id'] == book_id]
-    return jsonify(result)
+@app.route('/v1/collections/<string:name>/books', methods=['GET'])
+def api_collection_books(name):
+    queryset = Book.query.filter_by(collection=name).all()
+    results = [x.serialize() for x in queryset]
+    return jsonify(results)
 
+@app.route('/v1/collections/<string:collection_name>/books/<int:book_id>/hadiths', methods=['GET'])
+def api_hadiths(collection_name, book_id):
+    queryset = Hadith.query.filter_by(collection=collection_name, bookID=book_id).all()
+    results = [x.serialize() for x in queryset]
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- models.py
+++ models.py
@@ -1,26 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from main import app
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
+db.reflect()
 
 class HadithCollection(db.Model):
     __tablename__ = 'Collections'
-    collectionID = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    collectiontype = db.Column('type', db.String(30), nullable=False)
-    englishTitle = db.Column(db.String(200), nullable=False)
-    arabicTitle = db.Column(db.String(400), nullable=False)
-    hasvolumes = db.Column(db.String(3), nullable=False)
-    hasbooks = db.Column(db.String(3), nullable=False)
-    haschapters = db.Column(db.String(3), nullable=False, default='yes')
-    numhadith = db.Column(db.Integer, nullable=False)
-    totalhadith = db.Column(db.Integer)
-    englishgrade1 = db.Column(db.String(100))
-    arabicgrade1 = db.Column(db.String(200), nullable=False)
-    annotation = db.Column(db.String(300))
-    shortintro = db.Column(db.Text, nullable=False)
-    about = db.Column(db.Text, nullable=False)
-    status = db.Column(db.String(50), nullable=False)
-    numberinginfodesc = db.Column(db.Text, nullable=False)
 
     def serialize(self):
         return {
@@ -32,4 +17,41 @@ class HadithCollection(db.Model):
             'total_hadith': self.totalhadith,
             'short_intro': self.shortintro,
             'status': self.status
+        }
+
+class Book(db.Model):
+    __tablename__ = 'BookData'
+
+    def serialize(self):
+        return {
+            'collection': self.collection,
+            'book_id': self.ourBookID,
+            'book_id_en': str(self.englishBookID),
+            'book_number_en': self.englishBookNumber,
+            'book_name_en': self.englishBookName,
+            'book_id_ar': str(self.arabicBookID),
+            'book_number_ar': self.arabicBookNumber,
+            'book_name_ar': self.arabicBookName,
+            'first_number': self.firstNumber,
+            'last_number': self.lastNumber,
+            'total_number': self.totalNumber
+        }
+
+class Hadith(db.Model):
+    __tablename__ = 'HadithTable'
+
+    def serialize(self):
+        return {
+            'collection': self.collection,
+            'book_id': self.bookID,
+            'bab_number': str(self.babNumber),
+            'hadith_number': self.hadithNumber,
+            'our_hadith_number': self.ourHadithNumber,
+            'urn_en': self.englishURN,
+            'bab_name_en': self.englishBabName,
+            'text_en': self.englishText,
+            'urn_ar': self.arabicURN,
+            'bab_name_ar': self.arabicBabName,
+            'text_ar': self.arabicText,
+            'xrefs': self.xrefs
         }
