0e22acb9aef977b5de4d6fc852826ebdc7908877
Glaisher <glaisher.wiki@gmail.com>
2020-06-06 23:09:52 +0500

    Implement books and hadiths API
    Added hadithTable dump as it was missing from initial
    dump so would need to import HadithTable first for this
    to work properly.

--- models.py
+++ models.py
@@ -18,3 +18,40 @@ class HadithCollection(db.Model):
             'short_intro': self.shortintro,
             'status': self.status
         }
+
+class Book(db.Model):
+    __tablename__ = 'BookData'
+
+    def serialize(self):
+        return {
+            'collection': self.collection,
+            'book_id': self.ourBookID,
+            'book_id_en': str(self.englishBookID),
+            'book_number_en': self.englishBookNumber,
+            'book_name_en': self.englishBookName,
+            'book_id_ar': str(self.arabicBookID),
+            'book_number_ar': self.arabicBookNumber,
+            'book_name_ar': self.arabicBookName,
+            'first_number': self.firstNumber,
+            'last_number': self.lastNumber,
+            'total_number': self.totalNumber
+        }
+
+class Hadith(db.Model):
+    __tablename__ = 'HadithTable'
+
+    def serialize(self):
+        return {
+            'collection': self.collection,
+            'book_id': self.bookID,
+            'bab_number': str(self.babNumber),
+            'hadith_number': self.hadithNumber,
+            'our_hadith_number': self.ourHadithNumber,
+            'urn_en': self.englishURN,
+            'bab_name_en': self.englishBabName,
+            'text_en': self.englishText,
+            'urn_ar': self.arabicURN,
+            'bab_name_ar': self.arabicBabName,
+            'text_ar': self.arabicText,
+            'xrefs': self.xrefs
+        }
