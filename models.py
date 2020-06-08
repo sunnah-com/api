986808ebd11f0465f485904d6addb9dab1eae16b
Ansari <sunnah@iman.net>
2020-06-07 22:53:21 -0700

    Pruning set of returned fields

--- models.py
+++ models.py
@@ -9,14 +9,14 @@ class HadithCollection(db.Model):
 
     def serialize(self):
         return {
-            'id': self.collectionID,
             'name': self.name,
+            'has_books': self.hasbooks,
             'title_en': self.englishTitle,
             'title_ar': self.arabicTitle,
             'num_hadith': self.numhadith,
             'total_hadith': self.totalhadith,
+            'has_chapters': self.haschapters,
             'short_intro': self.shortintro,
-            'status': self.status
         }
 
 class Book(db.Model):
@@ -24,13 +24,8 @@ class Book(db.Model):
 
     def serialize(self):
         return {
-            'collection': self.collection,
             'book_id': self.ourBookID,
-            'book_id_en': str(self.englishBookID),
-            'book_number_en': self.englishBookNumber,
             'book_name_en': self.englishBookName,
-            'book_id_ar': str(self.arabicBookID),
-            'book_number_ar': self.arabicBookNumber,
             'book_name_ar': self.arabicBookName,
             'first_number': self.firstNumber,
             'last_number': self.lastNumber,
@@ -46,12 +41,11 @@ class Hadith(db.Model):
             'book_id': self.bookID,
             'bab_number': str(self.babNumber),
             'hadith_number': self.hadithNumber,
-            'our_hadith_number': self.ourHadithNumber,
-            'urn_en': self.englishURN,
             'bab_name_en': self.englishBabName,
             'text_en': self.englishText,
             'urn_ar': self.arabicURN,
             'bab_name_ar': self.arabicBabName,
             'text_ar': self.arabicText,
-            'xrefs': self.xrefs
+            'grade_ar': self.arabicgrade1,
+            'grade_en': self.englishgrade1,
         }
