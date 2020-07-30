3e68746ad7c6916654e07037efd869be0bb4ab93
Sunnah.com <sunnah@iman.net>
2020-07-30 16:39:46 -0700

    Modifying script to take command-line argument instead

--- scripts/key-gen.sh
+++ scripts/key-gen.sh
@@ -2,10 +2,20 @@
 # This script requires aws-cli to be installed
 # Please visit https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 # After installation use 'aws configure' to setup the credentials and then execute the script
-echo -n "Please enter the name for the key: "
-read KEYNAME
-KEYINFO=`aws apigateway create-api-key --name ${KEYNAME} --enabled`
+
+if [ "$#" -eq 0 ]
+then
+    echo "This script takes one command-line argument: the name of the key to be created"
+    exit 1
+fi
+
+KEYNAME=$1
+KEYINFO=`aws apigateway create-api-key --name ${KEYNAME} --region us-west-2 --enabled 2>&1`
+# KEYVALUE=`aws apigateway get-api-key --api-key ${KEYNAME} --include-value --region us-west-2 | grep value | cut -d\" -f4 2>&1`
+echo "Attempting to run command aws apigateway create-api-key --name ${KEYNAME} --region us-west-2 --enabled 2>&1"
+# echo "Command returned ${KEYINFO}"
 KEYID=`echo ${KEYINFO} | grep id | cut -d\" -f4`
 KEYVALUE=`echo ${KEYINFO} | grep value | cut -d\" -f8`
 aws apigateway create-usage-plan-key --usage-plan-id b3rk95 --key-type API_KEY --key-id ${KEYID} > /dev/null
-echo "New key successfully created: ${KEYVALUE}"
+echo -e "\nNew key successfully created: ${KEYVALUE}"
+
