f07cd24d716c0e71d3f9a573f8b0129e02e375f8
Sunnah.com <sunnah@iman.net>
2020-08-12 18:37:11 -0700

    key-gen script: Adding region to second command

--- scripts/key-gen.sh
+++ scripts/key-gen.sh
@@ -16,6 +16,6 @@ echo "Attempting to run command aws apigateway create-api-key --name ${KEYNAME} 
 # echo "Command returned ${KEYINFO}"
 KEYID=`echo ${KEYINFO} | grep id | cut -d\" -f4`
 KEYVALUE=`echo ${KEYINFO} | grep value | cut -d\" -f8`
-aws apigateway create-usage-plan-key --usage-plan-id b3rk95 --key-type API_KEY --key-id ${KEYID} > /dev/null
+aws apigateway create-usage-plan-key --usage-plan-id b3rk95 --key-type API_KEY --key-id ${KEYID} --region us-west-2 2>&1
 echo -e "\nNew key successfully created: ${KEYVALUE}"
 
