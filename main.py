c1bbea420293ac9a2e364579620fca5ed0912c2b
Muhammad Hasan Khan <slashdot10@gmail.com>
2020-09-06 23:38:10 -0700

    Remove unverified books from books collection
    Also removed the unused /hadiths[GET] end point

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask, jsonify, request, abort
 from sqlalchemy import func, or_
 from werkzeug.exceptions import HTTPException
 
-
 app = Flask(__name__)
 app.config.from_object("config.Config")
 
@@ -72,14 +71,14 @@ def api_collection(name):
 @app.route("/v1/collections/<string:name>/books", methods=["GET"])
 @paginate_results
 def api_collection_books(name):
-    return Book.query.filter_by(collection=name).order_by(func.abs(Book.ourBookID))
+    return Book.query.filter_by(collection=name, status=4).order_by(func.abs(Book.ourBookID))
 
 
 @app.route("/v1/collections/<string:name>/books/<string:bookNumber>", methods=["GET"])
 @single_resource
 def api_collection_book(name, bookNumber):
     book_id = Book.get_id_from_number(bookNumber)
-    return Book.query.filter_by(collection=name, ourBookID=book_id)
+    return Book.query.filter_by(collection=name, status=4, ourBookID=book_id)
 
 
 @app.route("/v1/collections/<string:collection_name>/books/<string:bookNumber>/hadiths", methods=["GET"])
@@ -106,27 +105,6 @@ def api_collection_book_chapters(collection_name, bookNumber):
 def api_collection_book_chapter(collection_name, bookNumber, chapterId):
     book_id = Book.get_id_from_number(bookNumber)
     return Chapter.query.filter_by(collection=collection_name, arabicBookID=book_id, babID=chapterId)
-
-
-@app.route("/v1/hadiths", methods=["GET"])
-@paginate_results
-def api_hadiths():
-    collection = request.args.get("collection")
-    bookNumber = request.args.get("bookNumber")
-    babId = request.args.get("chapterId")
-    hadithNumber = request.args.get("hadithNumber")
-
-    queryset = Hadith.query
-    if hadithNumber:
-        queryset = queryset.filter_by(hadithNumber=hadithNumber)
-    if babId:
-        queryset = queryset.filter_by(babID=float(babId))
-    if bookNumber:
-        queryset = queryset.filter_by(bookNumber=bookNumber)
-    if collection:
-        queryset = queryset.filter_by(collection=collection)
-
-    return queryset
 
 
 @app.route("/v1/hadiths/<int:urn>", methods=["GET"])
